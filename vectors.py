93e685c8e2823648f776a19931697f07504b9b0e
cavilosa <cavilosa33@gmail.com>
2021-03-02 11:43:25 -0700

    adding, substructing and miltiplying functions are implemented on vectors in python

--- vectors.py
+++ vectors.py
@@ -9,7 +9,25 @@ class Vector:
     def __str__(self):
         return "Vector: {}".format(self.coordinates)
 
-v = Vector([8,-9])
-w = Vector([1,3])
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
 
-print v.plus(w)
+    def times_scalar(self, c):
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+v = Vector([8.218,-9.341])
+w = Vector([-1.129,2.111])
+print (v.plus(w))
+
+v = Vector([7.119, 8.215])
+w = Vector([-8.223, 0.878])
+print (v.minus(w))
+
+v = Vector([1.671, -1.012, -0.318])
+c = 7.41
+print (v.times_scalar(c))
